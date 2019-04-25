22d847d394571485f3d78fedb51f6c7782d46f7e
joginder <joginderpc@gmail.com>
2019-04-25 15:53:12 +0530

    added code to new files

---
+++ helper.js
@@ -0,0 +1,18 @@
+// working with js
+var joginder = "joginder singh";
+var ankit = joginder.replace('joginder singh', 'ankit singh');
+
+// checking how it works on console.
+console.log(joginder);
+console.log(ankit);
+
+// working more with replace.
+var awesomeThoughts = "Im awesome man";
+var newThoughts = awesomeThoughts.replace('awesome', 'cool');
+
+$('#main').append(newThoughts);
+
+// formatted name
+var formattedName = HTMLHeadingElement.replace('%data%', name);
+
+$('header').append(formattedName);
